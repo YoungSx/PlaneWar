77424aab3ae46f63c08ed0852992db90cdd799f4
YoungSx <shangxin9515@outlook.com>
2015-05-07 20:08:18 +0800

    play on the PC

--- Files/script.js
+++ Files/script.js
@@ -7,16 +7,33 @@
 var mycanvas=document.getElementById("mycanvas");
 var ctx=mycanvas.getContext('2d');
 var image=new Image();
-var speed=0,gameOver=0,timeGap=800,sx=0,dy=80,direction=0;;
+var speed=0,gameOver=0,timeGap=500,sx=0,dy=80,direction=0;;
 var lastPos=new Array(0,0,0,0);
 var Bullet=new Array(new Array());
-var grates=0;
+var grates=0,x1=100,tmpNum=-1;
 image.src="Images/plane.png";
 window.addEventListener('deviceorientation', direUpdate, true); //重力传感器 方向
+
+var explosion=document.createElement("audio");
+explosion.src="Files/explosion.ogg";
+document.getElementById("body").appendChild(explosion);
+
+var shoot=document.createElement("audio");
+shoot.src="Files/shoot.ogg";
+document.getElementById("body").appendChild(shoot);
+
 function direUpdate(event){
 	e=event;
 	speed=e.gamma;
 }
+document.onmousemove =getMousePos;
+function getMousePos(event) {              
+	var e = event || window.event;              
+	x1=e.clientX;
+	ctx.clearRect(lastPos[0],lastPos[1],lastPos[2],lastPos[3]);//清除飞机刚才位置留影
+	ctx.drawImage(image,sx,167,65,dy,x1,400,65,dy);	
+	lastPos=[x1,400,65,dy];
+ } 
 function move(){
 	if(sx==0){
 		sx=64;
@@ -27,46 +44,61 @@ function move(){
 	}
 }
 function gameOverUI(x,y){
+	ctx.clearRect(0,0,mycanvas.width,mycanvas.height);
+	grates=0;
 	ctx.font="20px Georgia";
-	ctx.clearRect(0,0,mycanvas.width,mycanvas.height);
 	ctx.fillText("游戏结束！",120,250);
+	document.getElementById("restart").style.display="inline-block";
 }
-function refresh(){
+function restart(){
+	gameOver=1;
 	ctx.clearRect(0,0,mycanvas.width,mycanvas.height);
+	document.getElementById('grates').innerHTML="得分：0";
 	gameOver=0;
 	Bullet=new Array();
 	initialize();
 }
 function explosionAudio(){
-	var explosion=document.createElement("audio");
-	explosion.src="Files/explosion.ogg";
-	document.getElementById("body").appendChild(explosion);
 	explosion.play();
 }
 function shootAudio(){
-	var shoot=document.createElement("audio");
-	shoot.src="Files/shoot.ogg";
-	document.getElementById("body").appendChild(shoot);
 	shoot.play();
+}
+function enemyDie(x,y){//////////////////////////////
+	ctx.drawImage(image,145,93,37,37,x,y,37,27);//画出爆炸效果
+	grates+=100;
+	document.getElementById('grates').innerHTML="得分："+grates;
+	ctx.clearRect(x-1,y,38,27)
+	//setTimeout(function(){ctx.clearRect(x-1,y,38,27)},500);
 }
 function checkEnemyDie(x,y){
 	for(var i=0;i<Bullet.length;i++){
-		//console.log('('+Bullet[i][0]+','+Bullet[i][1]+')  ('+x+','+y+')');
-
+		if(Bullet[i][0]+7>x){
+			if(Bullet[i][0]<x+37){
+				if(Bullet[i][1]<y+27){
+					if(Bullet[i][1]>y){
+						enemyDie(x,y);
+						tmpNum=i;
+						var a=Bullet.splice(i,1);
+						console.log("Bullet:("+a[0]+")");
+						return i;
+						break;
+					}
+				}
+			}
+		}
+		/*
 		if(Bullet[i][0]+7>x && Bullet[i][0]<x+37 && Bullet[i][1]<y+27 && Bullet[i][1]>y){//子弹射中
 			enemyDie(x,y);
-			Bullet.remove(i);
+			tmpNum=i;
+			var a=Bullet.splice(i,1);
+			console.log("Bullet:("+a[0]+")");
 			return i;
+			break;
 		}
+		*/
 	}
 	return -1;
-}
-function enemyDie(x,y){//////////////////////////////
-	ctx.clearRect(x-1,y,38,27);
-	ctx.drawImage(image,145,93,37,37,x,y,37,27);
-	grates+=100;
-	document.getElementById('grates').innerHTML="得分："+grates;
-	setTimeout(function(){ctx.clearRect(x-1,y,38,27)},500);
 }
 function drawEnemy(x,y){
 	if((x+30)>lastPos[0] && x<(lastPos[0]+60) && (y+20)>lastPos[1] && y<lastPos[1]+70){//飞机与敌军碰撞
@@ -80,7 +112,8 @@ function drawEnemy(x,y){
 		return(0);
 	};
 	if(checkEnemyDie(x,y)!=-1){///////////////////////////////////////////////////////////敌军被子弹打中
-		alert("打中了！");//console.log("_(:з」∠)_");
+		console.log("_(:з」∠)_ ("+x+","+y+")");
+		return(0);
 	}
 	ctx.clearRect(x-1,y,38,27);
 	if(gameOver==0)ctx.drawImage(image,202,88,37,27,x,y+5,37,27);
@@ -93,16 +126,22 @@ function createEnemy(){
 		setTimeout(function(){createEnemy()},timeGap);
 	}
 }
-function drawBullet(x,y,i){//画子弹
+function drawBullet(x,y,i){//画子弹////////////////////要让子弹消失也要改这里
+	var bushu=15,jiange=100;
 	Bullet[i]=[x,y];
-	ctx.clearRect(x-1,y+5,9,15);
+	ctx.clearRect(x-1,y+bushu,9,15);
 	//ctx.drawImage(image,113,0,7,15,lastPos[0]+31,lastPos[1]-18,7,15);//子弹
 	if(gameOver==0)ctx.drawImage(image,113,3,7,15,x,y,7,15);//子弹
+	if(i==tmpNum){
+		tmpNum=-1;
+		ctx.clearRect(x-1,y,9,15);
+		return 0;
+	}
 	if(y<=-15||gameOver==1){
 		Bullet.shift();//删除最前端子弹队列
 		return 1;
 	}
-	else setTimeout(function(){drawBullet(x,y-5,i)},100);
+	else setTimeout(function(){drawBullet(x,y-bushu,i)},jiange);
 }
 function createBullet(){//创建子弹
 
@@ -114,9 +153,17 @@ function createBullet(){//创建子弹
 		setTimeout(function(){createBullet()},550);
 	}
 }
-image.onload=initialize();
+image.onload=function(){
+	ctx.font="20px Georgia";
+	ctx.fillText("飞机大战",120,250);
+}
+function start(){
+	ctx.clearRect(0,0,mycanvas.width,mycanvas.height);
+	document.getElementById('grates').innerHTML="得分：0";
+	initialize();
+}
 function initialize(){
-	var x1=200,x2=64;
+	var x2=64;
 	setInterval(function(){move()},200);
 	setInterval(function(){//大飞机的移动
 		if(gameOver==1)return 0;
@@ -137,6 +184,4 @@ function initialize(){
 	///////////////////////截取位置    放置位置	
 	//ctx.drawImage(image,200,167,65,80,200,450,65,80);//plane1 state1
 	//ctx.drawImage(image,64,167,65,70,200,450,65,70);//plane1 state2
-	
-
 }
